cc163da6936cb9d09779fd51b70699fa3d0611a1
Alin Tomescu <tomescu.alin@gmail.com>
2025-03-11 14:41:58 -0400

    add pre-commit hook to try and compile circom if there are changes (#36)

--- dev_setup.sh
+++ dev_setup.sh
@@ -6,3 +6,9 @@ set -x
 export RESOURCES_DIR="$HOME/.local/share/aptos-prover-service"
 python3 ./scripts/prepare_setups.py
 ./prover/scripts/dev_setup.sh
+
+
+# Install pre-commit hook
+cp git-hooks/compile-circom-if-needed-pre-commit .git/hooks/pre-commit
+chmod +x .git/hooks/pre-commit
+echo "Installed pre-commit hook for circom compilation successfully!"
